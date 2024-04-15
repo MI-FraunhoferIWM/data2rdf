066fa6425072a08f87c27bf1197c5995d52a098a
Matthias Büschelberger <matthias.bueschelberger@iwm.fraunhofer.de>
2024-04-15 12:20:50 +0200

    update json mapping file

--- tests/csv_pipeline_test/test_pipeline.py
+++ tests/csv_pipeline_test/test_pipeline.py
@@ -1,5 +1,6 @@
-import os
 import json
+import os
+
 import pytest
 
 test_folder = os.path.dirname(os.path.abspath(__file__))
@@ -17,42 +18,49 @@ raw_data = os.path.join(working_folder, "data", "DX56_D_FZ2_WR00_43.TXT")
 expected = os.path.join(output_folder, "output_pipeline.ttl")
 
 parser_args = {"header_sep": "\t", "column_sep": "\t", "header_length": 20}
-metadata = {'Bemerkung': '',
-            'Datum/Uhrzeit': '44335.4',
-            'Kraftaufnehmer': 'Kraftaufnehmer_1',
-            'Maschinendaten': 'maschine_1',
-            'Messlänge Standardweg': 80,
-            'Probenbreite': 20.04,
-            'Probendicke': 1.55,
-            'Probenkennung 2': 'Probentyp_2',
-            'Probentyp': 'Probentyp_1',
-            'Projektname': 'proj_name_1',
-            'Projektnummer': '123456',
-            'Prüfer': 'abc',
-            'Prüfgeschwindigkeit': 0.1,
-            'Prüfinstitut': 'institute_1',
-            'Prüfnorm': 'ISO-XX',
-            'Temperatur': 22,
-            'Versuchslänge': 120,
-            'Vorkraft': 2,
-            'Wegaufnehmer': 'Wegaufnehmer_1',
-            'Werkstoff': 'Werkstoff_1'
- }
+metadata = {
+    "Bemerkung": "",
+    "Datum/Uhrzeit": "44335.4",
+    "Kraftaufnehmer": "Kraftaufnehmer_1",
+    "Maschinendaten": "maschine_1",
+    "Messlänge Standardweg": 80,
+    "Probenbreite": 20.04,
+    "Probendicke": 1.55,
+    "Probenkennung 2": "Probentyp_2",
+    "Probentyp": "Probentyp_1",
+    "Projektname": "proj_name_1",
+    "Projektnummer": "123456",
+    "Prüfer": "abc",
+    "Prüfgeschwindigkeit": 0.1,
+    "Prüfinstitut": "institute_1",
+    "Prüfnorm": "ISO-XX",
+    "Temperatur": 22,
+    "Versuchslänge": 120,
+    "Vorkraft": 2,
+    "Wegaufnehmer": "Wegaufnehmer_1",
+    "Werkstoff": "Werkstoff_1",
+}
+
 
 @pytest.mark.parametrize("extension", ["xlsx", "json", dict])
 def test_csv_pipeline(extension) -> None:
     from rdflib import Graph
 
-    from data2rdf import AnnotationPipeline, Parser, QuantityMapping, PropertyMapping
+    from data2rdf import (  # isort:skip
+        AnnotationPipeline,
+        Parser,
+        PropertyMapping,
+        QuantityMapping,
+    )
 
     if isinstance(extension, str):
+        mapping = os.path.join(
+            mapping_folder, f"tensile_test_mapping.{extension}"
+        )
 
-        mapping = os.path.join(mapping_folder, f"tensile_test_mapping.{extension}")
-    
     else:
-
         path = os.path.join(mapping_folder, "tensile_test_mapping.json")
-        with open(path, "r", encoding="utf-8") as file:
+        with open(path, encoding="utf-8") as file:
             mapping = json.load(file)
 
     pipeline = AnnotationPipeline(
@@ -82,6 +90,6 @@ def test_csv_pipeline(extension) -> None:
     expected_graph.parse(expected)
 
     # TODO: NODE hashes are differing with every pipeline run. Not dramatic but this test should be executed
-    #assert pipeline.graph.isomorphic(expected_graph) 
+    # assert pipeline.graph.isomorphic(expected_graph)
 
     assert pipeline.plain_metadata == metadata
