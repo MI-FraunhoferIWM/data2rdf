f2153eb7025abd5dcb6840049eee3fe3980fef89
Matthias Büschelberger <matthias.bueschelberger@iwm.fraunhofer.de>
2025-03-17 11:32:15 +0100

    add ability for special charaters in json path

--- data2rdf/parsers/utils.py
+++ data2rdf/parsers/utils.py
@@ -1,6 +1,7 @@
 """Data2RDF parser utilities"""
 
 import json
+import re
 import warnings
 from typing import TYPE_CHECKING
 
@@ -15,6 +16,7 @@ from data2rdf.models.mapping import (  # isort:skip
     RelationType,
 )
 
+ALLOW_PATTERN = r"[^a-zA-Z0-9\-._$[*\[\]]*"
 
 if TYPE_CHECKING:
     from typing import Any, Dict, List, Union
@@ -220,3 +222,29 @@ def _value_exists(value: "Any") -> bool:
         bool: True if the value exists and is valid, otherwise False.
     """
     return pd.notnull(value) and value != ""
+
+
+def _check_jsonpath(expression):
+    """
+    Adjusts the JSONPath expression for compatibility.
+
+    This function checks the provided JSONPath expression against a predefined
+    pattern and modifies it for compatibility by enclosing each segment of the
+    path in double quotes if any matches are found. This ensures that the
+    expression is properly formatted for further processing.
+
+    Args:
+        expression (str): The JSONPath expression to be checked and adjusted.
+
+    Returns:
+        str: The adjusted JSONPath expression with segments enclosed in double quotes
+             if any matches were found, otherwise returns the original expression.
+    """
+
+    matches = re.findall(ALLOW_PATTERN, expression)
+    matches = [match for match in matches if match]
+    if matches:
+        print(matches)
+        splitted = expression.split(".")
+        expression = ".".join([f'"{exp}"' for exp in splitted])
+    return expression
--- tests/abox/json_multiple_iris/test_multiple_iris.py
+++ tests/abox/json_multiple_iris/test_multiple_iris.py
@@ -114,6 +114,57 @@ EXPECTED_CUSTOM_RELATIONS = """
 """
 
 
+DATA_SPECIAL_CHARS = {
+    "testing-machine": [
+        {
+            "Description (foo)": "Some description",
+            "Identifier 123": "123",
+            "Material (öä§$%)": [
+                "https://example.materials-data.space/knowledge/material/material1",
+                "https://example.materials-data.space/knowledge/material/material2",
+            ],
+            "id": "0034d610-959d-4dd0-97f0-f3b36eeea5c4",
+            "slug": "testing-machine1",
+            "Name": "Testing machine1",
+        }
+    ]
+}
+
+MAPPING_SPECIAL_CHARS = [
+    {
+        "iri": [
+            "https://w3id.org/emmo#EMMO_ee0466e4_780d_4236_8281_ace7ad3fc5d2",
+            "https://example.org/0034d610-959d-4dd0-97f0-f3b36eeea5c4",
+        ],
+        "suffix": "slug",
+        "source": "testing-machine[*]",
+        "suffix_from_location": True,
+        "custom_relations": [
+            {
+                "object_location": "Description (foo)",
+                "relation": "http://www.w3.org/2000/01/rdf-schema#comment",
+                "relation_type": "property",
+            },
+            {
+                "object_location": "Identifier 123",
+                "relation": "https://w3id.org/emmo#EMMO_a592c856_4103_43cf_8635_1982a1e5d5de",
+                "relation_type": "data_property",
+            },
+            {
+                "object_location": "Material (öä§$%)",
+                "relation": "http://www.w3.org/2000/01/rdf-schema#relatedTo",
+                "relation_type": "object_property",
+            },
+            {
+                "object_location": "Name",
+                "relation": "http://www.w3.org/2000/01/rdf-schema#label",
+                "relation_type": "annotation_property",
+            },
+        ],
+    }
+]
+
+
 def test_multiple_iris_custom_relations():
     from rdflib import Graph
 
@@ -152,5 +203,26 @@ def test_multiple_iris():
     )
     expected_graph = Graph()
     expected_graph.parse(data=EXPECTED)
+
+    assert pipeline.graph.isomorphic(expected_graph)
+
+
+def test_special_chars():
+    from rdflib import Graph
+
+    from data2rdf import Data2RDF, Parser
+
+    pipeline = Data2RDF(
+        raw_data=DATA_SPECIAL_CHARS,
+        mapping=MAPPING_SPECIAL_CHARS,
+        parser=Parser.json,
+        parser_args={"expand_array": True},
+        config={
+            "base_iri": "https://example.materials-data.space/knowledge/testing-machine/",
+            "suppress_file_description": True,
+        },
+    )
+    expected_graph = Graph()
+    expected_graph.parse(data=EXPECTED_CUSTOM_RELATIONS)
 
     assert pipeline.graph.isomorphic(expected_graph)
